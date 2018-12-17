e915abb1a045b325e1563854413cd207d6cc606f
CORRADI EMILIE <catemi3@orange.fr>
2018-12-17 11:53:31 +0100

    MotorGame en cours

---
+++ include/motor_game/MotorGame.hpp
@@ -0,0 +1,27 @@
+#ifndef MOTOR_GAME_HPP
+#define MOTOR_GAME_HPP
+
+#pragma once
+
+namespace motor_game
+{
+    /// \class MotorGame
+    /// \brief manage the non-graphic game
+    class MotorGame{
+        public :
+            /* CONSTRUCTOR - DESTRUCTOR */
+            /// \brief constructor
+            MotorGame() = default;
+            ~MotorGame = default;
+            
+            /// \brief initialize the non-graphic game
+            void initGame();
+            /// \brief game loop
+            void playGame();
+        
+        private :
+            /* ATTRIBUTE */
+            std::list<Element> m_elements; /*!< contains all the Element.s */
+    };
+}
+#endif
