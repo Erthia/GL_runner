6afaccbed77ac3045e5436827cad979c54d64be6
CORRADI EMILIE <catemi3@orange.fr>
2018-12-18 17:54:41 +0100

    laurine: collide gauche et droit ok

--- src/motor_game/Character.cpp
+++ src/motor_game/Character.cpp
@@ -113,10 +113,6 @@ void Character::scanList(std::list<Element> &list, const char &movement){
 	}*/
 
 bool Character::scanArray(PrintableElement (*list)[50][50], const char &movement) {
-		int x=this->getX();
-		int y=this->getY();
-		int z=this->getZ();
-		PrintableElement tmpElt = list[x][y][z];
 
 		/*list[x][y][z].printElement();
 		this->printElement();
@@ -124,9 +120,15 @@ bool Character::scanArray(PrintableElement (*list)[50][50], const char &movement
 		if(checkCollision(list[x][y][z]) == true)
 				std::cout << "HALLELUJAH" << std::endl;
 		else std::cout << "LA MOOOOOOOORT PUTAIIN" << std::endl;*/
+			int x=this->getX();
+			int y=this->getY();
+			int z=this->getZ()+1;
+			PrintableElement tmpElt;
 
 		switch(movement) {
 			case 'q' :
+			x-=1;
+			tmpElt = list[x][y][z];
 			if(abs((this->getX()-1) - tmpElt.getX()) < 1) {
 				if(abs(this->getY()- tmpElt.getY()) < 2) /// \our character's heighth is two
 				{
@@ -144,9 +146,24 @@ bool Character::scanArray(PrintableElement (*list)[50][50], const char &movement
 			if(checkCollisionMovement(list[x-1][y][z+1], movement) == true)
 				std::cout << "Collision scanArray !" << std::endl;*/
 			case 'd' :
-			if(checkCollisionMovement(list[x+1][y][z+1], movement) == true)
-				std::cout << "Collision scanArray !" << std::endl;
+			x+=1;
+				tmpElt = list[x][y][z];
+				if(abs((this->getX()+1) - tmpElt.getX()) < 1) {
+					if(abs(this->getY()- tmpElt.getY()) < 2) /// \our character's heighth is two
+					{
+						if(abs((this->getZ()+1)- tmpElt.getZ()) < 1)
+						{
+							std::cout << "HALLELUJAH" << std::endl;
+							return true;
+						}}}
+				else {
+					std::cout << "BOOOOOUH" << std::endl;
+					return false;
+				}
+				break;
+			
 		}
+		return false;
 
 		//list[x][y][z+1]
 		//std::cout << "NON AU DEPLACEMENT A DROITE" << std::endl;
