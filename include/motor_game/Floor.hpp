d3c40d2304d45549828be709f09553cac91069e5
CORRADI EMILIE <catemi3@orange.fr>
2018-12-21 14:29:48 +0100

    Emilie : classe Turn créée, pour gérer les endroits où le Hero peut tourner

--- include/motor_game/Floor.hpp
+++ include/motor_game/Floor.hpp
@@ -1,26 +1,29 @@
+#ifndef FLOOR_HPP
+#define FLOOR_HPP
+
 #pragma once
 
 #include <iostream>
 #include <string>
 #include "Element.hpp"
 
+namespace motor_game{
+    class Floor : public Element
+    {
+      public:
+        /// \brief default constructor of class Floor
+        Floor(); 
 
-// TO ERASE ???? we don't have to check floor collision
-class Floor : public Element
-{
-  public:
-  	/// default constructor of class Floor
-    Floor(); 
 
+        Floor(const glm::vec3 &position, const std::string &type = "Floor");
 
-    Floor(const glm::vec3 &position, const std::string &type = "Floor");
+        /// \brief method to display the value of Floor's attributes
+        void printElement() const;
 
-	/// brief method to display the value of Floor's attributes
-    void printElement() const;
+         /// \brief default destructor of our Floor
+        ~Floor(); 
 
-     /// default destructor of our Floor
-    ~Floor(); 
+    };
+}
 
-// TO ADD :  collide ??
-
-};
+#endif
